dc3f6ded397ad2de2e202b560484544b4d789d7d
private-yusuke <townstock@ksf.biglobe.ne.jp>
2018-09-19 20:48:04 +0900

    Fixed bug and made a new feature for onlyOneDatabase.js

--- database/onlyOneDatabase.js
+++ database/onlyOneDatabase.js
@@ -1,14 +1,17 @@
 const fs = require('fs')
+const moment = require('moment')
 
 /**
  * 一つのデータベースのファイルで管理します。
- * データが肥大化していきます。
+ * configでmaxSizeを0に設定すると、無制限でデータベースを拡張しつづけます。
+ * それ以外の場合、maxSizeバイトの大きさになったら、データベースを保管し、初期化します。
  * @prop {number} unsavedPostCount this.config.saveFrequency 回の投稿ごとに、データベースに保存します。
  */
 class OnlyOneDatabase {
   constructor (markov, config) {
     this.markov = markov
     this.config = config
+    this.unsavedPostCount = 0
   }
   databaseSync () {
     if (this.unsavedPostCount >= this.config.database.saveFrequency) {
@@ -27,6 +30,15 @@ class OnlyOneDatabase {
     fs.writeFile(this.config.database.path, this.markov.exportDatabase(), 'utf-8', function () {
       console.log('database successfully saved')
     })
+    if (this.config.database.maxSize !== 0) {
+      const size = fs.statSync(this.config.database.path).size
+      if (size >= this.config.database.maxSize) {
+        console.log(`database is too big. max = ${this.config.database.maxSize} <= size = ${size}.`)
+        console.log('renaming the database file.')
+        fs.renameSync(this.config.database.path, `${this.config.database.path}-${moment().unix()}.json`)
+        this.markov.loadDatabase('{}')
+      }
+    }
   }
 }
 
